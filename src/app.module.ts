c6fb78a021585eef9e4ebf621dad72c77bb2b3f8
sohee-an <ansoso634@gmail.com>
2023-10-11 16:03:53 +0900

    ✨ init

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_ROOT),
+      username: 'root',
+      password: process.env.DATABASE_PASSWORD,
+      database: 'travel',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
